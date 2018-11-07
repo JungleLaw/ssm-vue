1d9e73de9957209d679ac532a68f0bc305825448
Jungle Law <Junglelaw@live.com>
2018-11-08 01:53:23 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <!--<router-view/>-->
+    <!--<div>
+      <tr>
+        <td>username:</td>
+        <td><input type="text"/></td>
+      </tr>
+      <tr>
+        <td>password:</td>
+        <td><input type="text"/></td>
+      </tr>
+    </div>-->
+    <Login></Login>
+  </div>
+
+</template>
+
+<script>
+  /*export default {
+    name: 'App'
+  }*/
+  // import Hello from './components/HelloWorld'
+  //
+  // export default {
+  //   components: {
+  //     Hello
+  //   }
+  // }
+  import Login from './components/Login'
+
+  export default {
+    components: {
+      Login
+    }
+  }
+</script>
+
+<style>
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+</style>
---
+++ src/components/Login.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <!--<form method="get" action="http://localhost:8080/ssm/user/insert">-->
+    <form @submit.prevent="submit">
+      <table>
+        <tr>
+          <td><h1>{{username}}</h1></td>
+        </tr>
+        <tr>
+          <td><h1>{{password}}</h1></td>
+        </tr>
+        <tr>
+          <td>username:</td>
+          <td><input v-model="username" type="text" name="username"/></td>
+        </tr>
+        <tr>
+          <td>password:</td>
+          <td><input v-model="password" type="password" name="password"/></td>
+        </tr>
+      </table>
+      <input type="submit" value="提交">
+    </form>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Login',
+    data() {
+      return {
+        msg: 'Welcome to Your Vue.js App',
+        username: 'username',
+        password: 'password'
+      }
+    },
+    methods: {
+      submit: function () {
+        console.log("submit");
+        console.log("username = " + this.username);
+        console.log("password = " + this.password);
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
